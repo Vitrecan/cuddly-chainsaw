6a8d8897463aab626dd8c3388a79f93625a95128
Vitrecan <vitrecan@gmail.com>
2024-01-14 02:08:32 +1100

    Created index.html with two buttons and separated CSS and JavaScript into their own files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("button1").onclick = function () {
+    location.href="file1.html";
+};
+
+document.getElementById("button2").onclick = function () {
+    location.href = "file2.html";
+};
